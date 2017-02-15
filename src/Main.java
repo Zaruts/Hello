2f831e3f1688a411295fa7c6f9acc1fdd3a784bb
Zaruts <yakalulu@gmail.com>
2017-02-15 17:12:25 +0100

    ajout d'une fonctionnalité qui permet de rentrer son prénom

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello !");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Comment vous appelez-vous ? ");
+		String name = sc.nextLine();
+		System.out.println("Bonjour " +name +" ! ");
+		
 	}
 
 }
